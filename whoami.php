0c58786a1b73c2dc393f41acb5c85efee54039ae
Ivan Nikitin <vantoozz@gmail.com>
2017-07-01 20:19:19 +0300

    + getallheaders

--- whoami.php
+++ whoami.php
@@ -6,3 +6,16 @@ echo json_encode([
     'headers' => getallheaders(),
     'remote_address' => $_SERVER['REMOTE_ADDR']??null,
 ]);
+
+if (!function_exists('getallheaders')) {
+    function getallheaders()
+    {
+        $headers = [];
+        foreach ($_SERVER as $name => $value) {
+            if (0 === strpos($name, 'HTTP_')) {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
+}
