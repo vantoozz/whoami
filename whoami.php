a79e6e324d7773aa46a30e31b095ab0395686d2f
Ivan Nikitin <vantoozz@gmail.com>
2017-07-01 20:20:21 +0300

    + getallheaders

--- whoami.php
+++ whoami.php
@@ -1,12 +1,5 @@
 <?php declare(strict_types = 1);
 
-echo json_encode([
-    'post' => $_POST,
-    'get' => $_GET,
-    'headers' => getallheaders(),
-    'remote_address' => $_SERVER['REMOTE_ADDR']??null,
-]);
-
 if (!function_exists('getallheaders')) {
     function getallheaders()
     {
@@ -19,3 +12,10 @@ if (!function_exists('getallheaders')) {
         return $headers;
     }
 }
+
+echo json_encode([
+    'post' => $_POST,
+    'get' => $_GET,
+    'headers' => getallheaders(),
+    'remote_address' => $_SERVER['REMOTE_ADDR']??null,
+]);
